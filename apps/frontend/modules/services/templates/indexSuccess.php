82005c7dab39344a767353f9f650e11fdb2e34d4
volodymyr <kogeva29898@gmail.com>
2013-04-16 03:14:08 +0300

    add correct code

--- apps/frontend/modules/services/templates/indexSuccess.php
+++ apps/frontend/modules/services/templates/indexSuccess.php
@@ -24,3 +24,34 @@
     </div>
     <div class="bottom-teeth-border-decorator"></div>
 </div>
+<script type="text/javascript">
+
+    $('.sevices-entity').bind('click', function () {
+        sourceText = $(this).find('.source-text').val();
+        cutedText  = $(this).find('.cuted-text').val();
+        $mainText  = $(this).find('.entity-text');
+
+        if($($mainText).hasClass('deployed'))
+        {
+            $($mainText).removeClass('deployed').html(cutedText);
+        }
+        else
+            $($mainText).addClass('deployed').html(sourceText);
+    });
+
+    $('.services-category-filter-buttons .architect').one('click', function() {
+        $('.load-data-container').load('/services/filter-by-categoty?name=architect');
+    });
+
+    $('.services-category-filter-buttons .design').one('click', function() {
+        $('.load-data-container').load('/services/filter-by-categoty?name=design');
+    });
+
+    $('.services-category-filter-buttons .development').one('click', function() {
+        $('.load-data-container').load('/services/filter-by-categoty?name=development');
+    });
+
+    $('.services-category-filter-buttons .grow').one('click', function() {
+        $('.load-data-container').load('/services/filter-by-categoty?name=grow');
+    });
+</script>
