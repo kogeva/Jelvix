cf1fd89ed70aa8c51ae48cfe672d5b5a10f25fed
vova <kogeva19898@gmail.com>
2013-05-28 00:52:55 +0300

    add new changes

---
+++ apps/backend/modules/request/actions/actions.class.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once dirname(__FILE__).'/../lib/requestGeneratorConfiguration.class.php';
+require_once dirname(__FILE__).'/../lib/requestGeneratorHelper.class.php';
+
+/**
+ * request actions.
+ *
+ * @package    Jelvix
+ * @subpackage request
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class requestActions extends autoRequestActions
+{
+}
---
+++ apps/backend/modules/request/lib/requestGeneratorConfiguration.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * request module configuration.
+ *
+ * @package    Jelvix
+ * @subpackage request
+ * @author     Your name here
+ * @version    SVN: $Id: configuration.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class requestGeneratorConfiguration extends BaseRequestGeneratorConfiguration
+{
+}
