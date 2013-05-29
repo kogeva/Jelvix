07c97f1d5a73aac93d991fcb7719b62981ab6d71
vova <kogeva19898@gmail.com>
2013-05-30 02:38:44 +0300

    add new changes

--- apps/frontend/modules/services/templates/_servicesEntity.php
+++ apps/frontend/modules/services/templates/_servicesEntity.php
@@ -3,14 +3,13 @@
         <li class="services-list-element">
             <ul class="services-list-element-container">
                 <?php foreach ($service as $item): ?>
-                    <?php $smallText = substr(sfOutputEscaperGetterDecorator::unescape($item->getText()), 0, rand(150, 300)) ?>
+                    <?php $smallText = substr(html_entity_decode($item->getText()), 0, rand(150, 300)) ?>
                     <li class="cdscadscdsca">
                         <div class="services-entity-container">
                             <div class="sevices-entity">
                                 <div
                                     class="border-top category-<?php echo $item->getJelvixServicesCategory()->getName() ?>-top"></div>
-                                <div
-                                    class="cervices-entity-body category-<?php echo $item->getJelvixServicesCategory()->getName() ?>-body">
+                                <div class="cervices-entity-body category-<?php echo $item->getJelvixServicesCategory()->getName() ?>-body">
                                     <div class="cervices-entity-head ">
                                         <?php echo $item->getTitle() ?>
                                     </div>
