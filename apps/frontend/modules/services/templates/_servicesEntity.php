9963fc9d0b807bf851b355a6847b59ef5efd2e16
vova <kogeva19898@gmail.com>
2013-05-30 02:33:28 +0300

    add new changes

--- apps/frontend/modules/services/templates/_servicesEntity.php
+++ apps/frontend/modules/services/templates/_servicesEntity.php
@@ -3,7 +3,7 @@
         <li class="services-list-element">
             <ul class="services-list-element-container">
                 <?php foreach ($service as $item): ?>
-                    <?php $smallText = substr($item->getText(), 0, rand(150, 300)) ?>
+                    <?php $smallText = substr(sfOutputEscaperGetterDecorator::unescape($item->getText()), 0, rand(150, 300)) ?>
                     <li class="cdscadscdsca">
                         <div class="services-entity-container">
                             <div class="sevices-entity">
@@ -15,7 +15,7 @@
                                         <?php echo $item->getTitle() ?>
                                     </div>
                                     <div class="entity-text category-architect">
-                                        <?php echo sfOutputEscaper::unescape($smallText) . '...' ?>
+                                        <?php echo $smallText . '...' ?>
                                     </div>
                                     <input type="hidden" class="source-text" value="<?php echo $item->getText() ?>">
                                     <input type="hidden" class="cuted-text" value="<?php echo $smallText ?>">
