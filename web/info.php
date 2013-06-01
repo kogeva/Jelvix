905f18f8f13b88f12e636168c13e617cc8cda8f3
vova <kogeva19898@gmail.com>
2013-06-01 03:08:10 +0300

    add new changes

---
+++ web/info.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: vova
+ * Date: 01.06.13
+ * Time: 3:05
+ * To change this template use File | Settings | File Templates.
+ */
+
+echo phpinfo();
