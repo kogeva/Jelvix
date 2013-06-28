d1a5314fb6d6b0b60e3cf4aea15044afec3978cd
vova <kogeva19898@gmail.com>
2013-06-28 23:13:55 +0300

    Add new functional

---
+++ apps/backend/modules/module/actions/actions.class.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once dirname(__FILE__).'/../lib/moduleGeneratorConfiguration.class.php';
+require_once dirname(__FILE__).'/../lib/moduleGeneratorHelper.class.php';
+
+/**
+ * module actions.
+ *
+ * @package    Jelvix
+ * @subpackage module
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class moduleActions extends autoModuleActions
+{
+}
